198bfab288a7fbc1b7288e705269ad17f266e077
JotaPeans <jpfontesferreira1@gmail.com>
2023-11-08 16:44:41 -0300

    set to unable art

--- src/app/view/art/[id]/page.tsx
+++ src/app/view/art/[id]/page.tsx
@@ -1,25 +1,25 @@
-"use client"
+// "use client"
 
-import { useRouter } from "next/navigation";
-import Image from "next/image";
+// import { useRouter } from "next/navigation";
+// import Image from "next/image";
 
-import data from "@/lib/data";
-import Button from "@/app/components/Button";
-import { ChevronDown, ChevronUp, ChevronLeft, Play, Pause } from "lucide-react";
-import Title from "@/app/components/Title";
-import useKey from "@/hooks/useKey";
+// import data from "@/lib/data";
+// import Button from "@/app/components/Button";
+// import { ChevronDown, ChevronUp, ChevronLeft, Play, Pause } from "lucide-react";
+// import Title from "@/app/components/Title";
+// import useKey from "@/hooks/useKey";
 
 const Art = ({ params }: { params: { id: string } }) => {
-    const router = useRouter();
-    const artData = data.find(d => d.id === parseInt(params.id) && d.type === "art");
+    // const router = useRouter();
+    // const artData = data.find(d => d.id === parseInt(params.id) && d.type === "art");
 
-    if(!artData) router.push("/");
+    // if(!artData) router.push("/");
 
-    useKey("a", () => router.push("/")); // tecla pra ir pra direita
+    // useKey("a", () => router.push("/")); // tecla pra ir pra direita
         
     return (
         <main className="w-full min-h-screen bg-zinc-200 flex flex-col items-center justify-center gap-5">
-            <Title className="text-5xl text-zinc-700">{artData?.name}</Title>
+            {/* <Title className="text-5xl text-zinc-700">{artData?.name}</Title>
             <h2 className="text-lg font-semibold text-zinc-600">Por: {artData?.author}</h2>
 
             <div className="flex flex-col items-center justify-center gap-5 p-5 bg-white rounded-2xl w-1/2">
@@ -29,7 +29,7 @@ const Art = ({ params }: { params: { id: string } }) => {
             <Button onClick={() => router.push("/")} className="gap-2 pl-7">
                 <ChevronLeft strokeWidth={2.2}/>
                 Voltar
-            </Button>
+            </Button> */}
         </main>
     );
 }
